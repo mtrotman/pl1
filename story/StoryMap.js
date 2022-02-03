48bf4c2d4f8f7150bf11cddcc40dfc678cde47a2
mtrotman <91693997+mtrotman@users.noreply.github.com>
2022-02-03 14:02:59 -0330

    Update StoryMap.js

--- story/StoryMap.js
+++ story/StoryMap.js
@@ -89,5 +89,26 @@ function initMap() {
         stylers: [{ color: "#17263c" }],
       },
     ],
+  }); 
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  const markers = locations.map((location, i) => {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+  // Add a marker clusterer to manage the markers.
+  new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
 }
+const locations = [
+  { lat: 13.247780, lng: -59.650200 },
+  { lat: 51.104, lng: -94.288},
+  ];
